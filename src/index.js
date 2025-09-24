831b775ab8619b0531155e32e8b999137b64b0a6
Tjay05 <tjayolatayo@gmail.com>
2025-09-24 14:41:14 +0100

    Add fetch function for current forecasts

--- src/index.js
+++ src/index.js
@@ -31,3 +31,65 @@ search.addEventListener('click', () => {
     searchDrop.classList.remove('drop-menu');
   }
 })
+
+// Location declarations
+const currentLocation = document.getElementById('location');
+const currentTime = document.getElementById('time');
+const currentTemp = document.getElementById('current-temp');
+const currentHum = document.getElementById('current-humidity');
+const currentWind = document.getElementById('current-wind');
+const currentPrecp = document.getElementById('current-precip');
+
+// Tumezone formatter 
+const formatTz = (tz) => {
+  const [region, city] = tz.split("/");
+  return `${city.replace(/_/g, " ")}, ${region}`;
+};
+
+// Date format function
+const formatDate = (rawDate) => {
+  const date = new Date(rawDate);
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+  return date.toLocaleDateString('en-US', options);
+}
+
+// Imperial fetch function
+window.onload = () => {
+  fetchData();
+}
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=9.9285&longitude=8.8921&daily=weather_code,apparent_temperature_max,apparent_temperature_min&hourly=apparent_temperature,weather_code&current=relative_humidity_2m,precipitation,wind_speed_10m,apparent_temperature,weather_code&timezone=auto&wind_speed_unit=mph&precipitation_unit=inch');
+    const data = await res.json();
+    if (res.ok) {
+      currentLocation.innerHTML = formatTz(data.timezone);
+      currentTime.innerHTML = formatDate(data.current.time);
+      currentTemp.innerHTML = data.current.apparent_temperature + '°';
+      currentHum.innerHTML = data.current.relative_humidity_2m + '%';
+      currentWind.innerHTML = data.current.wind_speed_10m + ' mph';
+      currentPrecp.innerHTML = Math.round(data.current.precipitation * 100)/100 + ' in';
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+// const fetchData = () => {
+//   fetch('https://api.open-meteo.com/v1/forecast?latitude=9.9285&longitude=8.8921&daily=weather_code,apparent_temperature_max,apparent_temperature_min&hourly=apparent_temperature,weather_code&current=relative_humidity_2m,precipitation,wind_speed_10m,apparent_temperature,weather_code&timezone=auto&wind_speed_unit=mph&precipitation_unit=inch')
+//     .then(function(res){
+//       return res.json()
+//     })
+//     .then(function(data){
+//       console.log(data.timezone)
+//     })
+//     .catch(function(err){
+//       console.log(err)
+//     })
+// }
